2bfdb8dda8b1d4697a2bfbfb91c23183a8409793
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2023-02-25 17:16:18 +0530

    get it working

---
+++ main.ts
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from "npm:openai@3";
+import "https://deno.land/std@0.178.0/dotenv/load.ts";
+import { serve } from "https://deno.land/std@0.175.0/http/server.ts";
+
+const apiKey = Deno.env.get("API_KEY");
+
+const getCompletion = async (prompt: string, token: number) => {
+    const configuration = new Configuration({
+        apiKey: apiKey,
+    });
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: prompt,
+        temperature: 0.8,
+        max_tokens: token,
+    });
+    return response.data.choices;
+};
+
+async function handler(req: Request): Promise<Response> {
+    const data = await req.json();
+    console.log(data);
+    const prompt = "give me notes on the following material " + data.content;
+
+    const response = await getCompletion(prompt, 100);
+
+    return new Response(response[0].text);
+}
+
+console.log("Listening on http://localhost:8000");
+serve(handler);
